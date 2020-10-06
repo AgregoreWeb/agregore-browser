2f7ba8d9bad1120f0ebe4d7528648ecd2c32bcfa
Kyran <kyran@gostelow.me>
2020-10-06 18:40:53 +1100

    Cleanup

--- app/window/index.js
+++ app/window/index.js
@@ -203,16 +203,19 @@ class Window extends EventEmitter {
     this.web.on('new-window', (...args) => {
       this.emit('new-window', ...args)
     })
+
+    // Send to UI
     this.web.on('page-title-updated', (event, title) => {
       this.send('page-title-updated', title)
     })
-    this.window.once('ready-to-show', () => this.window.show())
     this.window.on('enter-html-full-screen', () => {
       this.send('enter-html-full-screen')
     })
     this.window.on('leave-html-full-screen', () => {
       this.send('leave-html-full-screen')
     })
+
+    this.window.once('ready-to-show', () => this.window.show())
     this.window.on('close', () => {
       this.web.destroy()
       this.emit('close')
