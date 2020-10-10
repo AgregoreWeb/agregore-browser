3af79b11a3012a7a439f285547e6102ece00c3d7
Kyran <kyran@gostelow.me>
2020-10-11 03:10:01 +1100

    Remove old Gemini code

--- app/protocols/gemini-protocol.js
+++ app/protocols/gemini-protocol.js
@@ -1,6 +1,4 @@
 const fetch = require('gemini-fetch')()
-const render = require('gemini-to-html/render')
-const parse = require('gemini-to-html/parse')
 const { Readable } = require('stream')
 const MIMEType = require('whatwg-mimetype')
 
@@ -27,50 +25,12 @@ module.exports = async function createHandler () {
       headers[key] = value
     }
 
-    const contentType = MIMEType.parse(headers['content-type'])
-    // Rewrite gemini pages to use custom renderer
-    if (contentType && contentType.essence === 'text/gemini') {
-      const pageText = await response.text()
-      const tokens = parse(pageText)
-
-      const content = render(tokens)
-
-      // Update the content type header to text/html while preserving parameters
-      contentType.type = 'text'
-      contentType.subtype = 'html'
-      headers['content-type'] = contentType.toString()
-
-      return sendResponse({
-        statusCode,
-        headers,
-        data: intoStream(generatePage(url, tokens, content))
-      })
-    }
-
     sendResponse({
       statusCode,
       headers,
       data
     })
   }
-}
-
-function generatePage (url, tokens, content) {
-  const title = tokens.find(({ type }) => type === 'header') || { content: url }
-  return `
-<!DOCTYPE html>
-<title>${title.content}</title>
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="mobile-web-app-capable" content="yes">
-<style>
-@import url("agregore://theme/style.css");
-</style>
-
-${content}
-`
 }
 
 function intoStream (data) {
