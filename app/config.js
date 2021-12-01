ab549e1db59c5fc967a80f4232a710ed73e01fff
tedd pasta <37799569+resession@users.noreply.github.com>
2021-12-01 10:41:52 -0600

    gun-fetch (#131)
    * add gun-fetch
    
    * add gun-fetch
    
    * install latest gun-fetch
    
    * install latest gun-fetch
    
    * fix hex encoding
    
    * fix encoding
    
    * add agregore storage folder
    
    * update
    
    * update
    
    * make change
    
    * make change
    
    * make change

--- app/config.js
+++ app/config.js
@@ -7,6 +7,22 @@ const DEFAULT_EXTENSIONS_DIR = path.join(USER_DATA, 'extensions')
 const DEFAULT_IPFS_DIR = path.join(USER_DATA, 'ipfs')
 const DEFAULT_HYPER_DIR = path.join(USER_DATA, 'hyper')
 const DEFAULT_BT_DIR = path.join(USER_DATA, 'bt')
+const DEFAULT_GUN_DIR = path.join(USER_DATA, 'gun')
+const DEFAULT_GUN_PEERS = ["https://gun-manhattan.herokuapp.com/gun",
+"https://us-west.xerberus.net/gun",
+"http://gun-matrix.herokuapp.com/gun",
+"https://gun-ams1.maddiex.wtf:443/gun",
+"https://gun-sjc1.maddiex.wtf:443/gun",
+"https://dletta.rig.airfaas.com/gun",
+"https://mg-gun-manhattan.herokuapp.com/gun",
+"https://gunmeetingserver.herokuapp.com/gun",
+"https://e2eec.herokuapp.com/gun",
+"https://gun-us.herokuapp.com/gun",
+"https://gun-eu.herokuapp.com/gun",
+"https://gunjs.herokuapp.com/gun",
+"https://www.raygun.live/gun",
+"https://gun-armitro.herokuapp.com/",
+"https://fire-gun.herokuapp.com/gun"]
 
 module.exports = require('rc')('agregore', {
   accelerators: {
@@ -52,5 +68,11 @@ module.exports = require('rc')('agregore', {
   // All options here: https://github.com/webtorrent/webtorrent/blob/master/docs/api.md
   btOptions: {
     storageLocation: DEFAULT_BT_DIR
+  },
+  // All options here: https://gun.eco/docs/API#-a-name-gun-a-gun-options-
+  gunOptions: {
+    file: DEFAULT_GUN_DIR,
+    peers: DEFAULT_GUN_PEERS,
+    radisk: true
   }
 })
