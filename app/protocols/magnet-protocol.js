fc101ea6a2d81d94c77bde812b8319a1cd60c90b
mauve <ranger@mauve.moe>
2021-12-20 15:22:17 -0500

    Added support for mutable torrents

--- app/protocols/magnet-protocol.js
+++ app/protocols/magnet-protocol.js
@@ -1,6 +1,7 @@
 const { Readable } = require('stream')
 
 const INFO_HASH_MATCH = /^urn:btih:([a-f0-9]{40})$/ig
+const PUBLIC_KEY_MATCH = /^urn:btpk:([a-f0-9]{64})$/ig
 
 module.exports = async function createHandler () {
   return function magnetHandler (req, sendResponse) {
@@ -8,26 +9,39 @@ module.exports = async function createHandler () {
       const parsed = new URL(req.url)
 
       const xt = parsed.searchParams.get('xt')
-      if (!xt) {
-        sendError('Magnet link has no `xt` parameter')
-      } else {
+      const xs = parsed.searchParams.get('xs')
+
+      if (xs) {
+        const match = PUBLIC_KEY_MATCH.exec(xs)
+        if (!match) {
+          return sendError('Magnet has no bittorrent infohash')
+        }
+        const publicKey = match[1]
+        const final = `bittorrent://${publicKey}`
+        sendFinal(final)
+      } else if (xt) {
         const match = INFO_HASH_MATCH.exec(xt)
         if (!match) {
-          sendError('Magnet has no bittorrent infohash')
-        } else {
-          const infohash = match[1]
-          const final = `bittorrent://${infohash}/`
-          sendResponse({
-            statusCode: 308,
-            headers: {
-              Location: final
-            },
-            data: intoStream('')
-          })
+          return sendError('Magnet has no bittorrent infohash')
         }
+        const infohash = match[1]
+        const final = `bittorrent://${infohash}/`
+        sendFinal(final)
+      } else {
+        return sendError('Magnet link has no `xt` or `xs` parameter')
       }
     } catch (e) {
       sendError(e.stack)
+    }
+
+    function sendFinal (Location) {
+      sendResponse({
+        statusCode: 308,
+        headers: {
+          Location
+        },
+        data: intoStream('')
+      })
     }
 
     function sendError (message) {
