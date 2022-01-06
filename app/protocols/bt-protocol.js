52b2be8324bc3509aeb17d083802b1fde33c4f87
RangerMauve <ranger@mauve.moe>
2022-01-06 13:35:02 -0500

    Added support for mutable torrents (#137)

--- app/protocols/bt-protocol.js
+++ app/protocols/bt-protocol.js
@@ -1,8 +1,9 @@
-const makeFetch = require('bt-fetch')
 const fetchToHandler = require('./fetch-to-handler')
 
 module.exports = async function createHandler (options, session) {
   return fetchToHandler(async () => {
+    const makeFetch = require('bt-fetch')
+
     const fetch = makeFetch(options)
 
     return fetch
