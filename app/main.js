bc745f408f0ed4dbc19da1173be72c5cf438559c
mauve <ranger@mauve.moe>
2022-06-03 15:35:00 -0400

    Use default windows-all-closed behavior

--- app/main.js
+++ app/main.js
@@ -81,15 +81,6 @@ windowManager.on('open', window => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(onready)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
