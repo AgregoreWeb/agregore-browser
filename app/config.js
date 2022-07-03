3d041bd8086122a66cc2695faf35fe87e8a04d06
av8ta <av8ta@protonmail.com>
2022-07-03 22:51:01 +0800

    bundle ssbd

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const USER_DATA = app.getPath('userData')
 const DEFAULT_EXTENSIONS_DIR = path.join(USER_DATA, 'extensions')
 const DEFAULT_IPFS_DIR = path.join(USER_DATA, 'ipfs')
 const DEFAULT_HYPER_DIR = path.join(USER_DATA, 'hyper')
-// const DEFAULT_SSB_DIR = path.join(USER_DATA, 'ssb')
+const DEFAULT_SSB_APPNAME = 'agregore-ssb'
 const DEFAULT_BT_DIR = path.join(USER_DATA, 'bt')
 const DEFAULT_GUN_DIR = path.join(USER_DATA, 'gun')
 
@@ -56,8 +56,16 @@ module.exports = require('rc')('agregore', {
   hyperOptions: {
     storage: DEFAULT_HYPER_DIR
   },
-  // All options here: https://github.com/ssbc/ssb-config#configuration
-  ssbOptions: {},
+  /**
+   * All ssb options here: https://github.com/ssbc/ssb-config#configuration
+   * For bundled ssb server we use ssbd. Pass in an array of require'd ssb plugins
+   */
+  ssbOptions: {
+    appname: DEFAULT_SSB_APPNAME,
+    ssbd: {
+      plugins: require('@metacentre/shipyard-ssb')
+    }
+  },
   // All options here: https://github.com/webtorrent/webtorrent/blob/master/docs/api.md
   btOptions: {
     storageLocation: DEFAULT_BT_DIR
