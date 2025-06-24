1736b4160afc6a1aa779c600fa463bbc4260ebb6
Mauve Signweaver <contact@mauve.moe>
2025-06-24 18:18:28 -0400

    fix: Use per-tab IDs for streaming AI for security

--- app/llm-preload.js
+++ app/llm-preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer, webFrame } = require('electron')
 
 const iteratorMaps = new Map()
-const iteratorId = 1
+let iteratorId = 1
 
 contextBridge.exposeInMainWorld('_agregoreLLM', {
   chat,
@@ -28,7 +28,7 @@ function setUpGlobal () {
         globalThis._agregoreLLM.chat(args).then(onResolve, onReject)
       },
       async * [Symbol.asyncIterator] () {
-        const { id } = await globalThis._agregoreLLM.chatStream(args)
+        const id = await globalThis._agregoreLLM.chatStream(args)
         try {
           while (true) {
             const { done, value } = await globalThis._agregoreLLM.iteratorNext(id)
@@ -48,7 +48,7 @@ function setUpGlobal () {
         globalThis._agregoreLLM.complete(prompt, args).then(onResolve, onReject)
       },
       async * [Symbol.asyncIterator] () {
-        const { id } = await globalThis._agregoreLLM.completeStream(prompt, args)
+        const id = await globalThis._agregoreLLM.completeStream(prompt, args)
         try {
           while (true) {
             const { done, value } = await globalThis._agregoreLLM.iteratorNext(id)
@@ -66,22 +66,34 @@ function setUpGlobal () {
 async function chat (args) {
   return ipcRenderer.invoke('llm-chat', args)
 }
-async function chatStream (args) {
-  return ipcRenderer.invoke('llm-chat-stream', args)
-}
 
 async function complete (prompt, args = {}) {
   return ipcRenderer.invoke('llm-complete', { prompt, ...args })
 }
 
-async function completeStream (prompt, args = {}) {
-  return ipcRenderer.invoke('llm-complete-stream', { prompt, ...args })
+async function chatStream (args) {
+  const { id } = await ipcRenderer.invoke('llm-chat-stream', args)
+  const localId = iteratorId++
+  iteratorMaps.set(localId, id)
+  return localId
 }
 
-async function iteratorNext (id) {
+async function completeStream (prompt, args = {}) {
+  const { id } = await ipcRenderer.invoke('llm-complete-stream', { prompt, ...args })
+  const localId = iteratorId++
+  iteratorMaps.set(localId, id)
+  return localId
+}
+
+async function iteratorNext (localId) {
+  const id = iteratorMaps.get(localId)
+  if (!id) throw new Error('Unknown iterator ID')
   return ipcRenderer.invoke('llm-iterate-next', { id })
 }
 
-async function iteratorReturn (id) {
+async function iteratorReturn (localId) {
+  const id = iteratorMaps.get(localId)
+  if (!id) throw new Error('Unknown iterator ID')
+  iteratorMaps.delete(localId)
   return ipcRenderer.invoke('llm-iterate-return', { id })
 }
