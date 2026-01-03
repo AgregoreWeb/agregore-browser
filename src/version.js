8a612b6ed59286c66547b6105630fc7fac785653
Mauve Signweaver <contact@mauve.moe>
2026-01-03 00:47:54 -0500

    fix: Rever package name from dev

---
+++ src/version.js
@@ -0,0 +1,33 @@
+
+export const name = 'agregore-browser'
+export const version = '2.20.0'
+export const dependencies = {
+  "@roamhq/wrtc": "^0.8.0",
+  "abort-controller": "^3.0.0",
+  "bt-fetch": "^3.1.1",
+  "compact-encoding": "2.18",
+  "create-desktop-shortcuts": "^1.7.0",
+  "data-uri-to-buffer": "^3.0.1",
+  "decompress": "^4.2.1",
+  "delay": "^6.0.0",
+  "electron-extended-webextensions": "^0.0.11",
+  "fs-extra": "^9.0.1",
+  "gemini-fetch": "^2.1.1",
+  "gemini-to-html": "^1.0.0",
+  "go-ipfs": "^0.22.0",
+  "hyper-sdk": "^6.2.1",
+  "hypercore-fetch": "^10.1.0",
+  "ipfs-http-client": "^60.0.0",
+  "ipfsd-ctl": "^13.0.0",
+  "js-ipfs-fetch": "^5.1.0",
+  "make-fetch": "^3.1.3",
+  "mime": "^2.4.6",
+  "multiformats": "^9.9.0",
+  "p-queue": "^7.3.4",
+  "rc": "^1.2.8",
+  "sanitize-filename": "^1.6.3",
+  "scoped-fs": "^1.4.1",
+  "semver": "^7.5.2",
+  "ssb-fetch": "^1.5.2",
+  "web3protocol": "^0.6.2"
+}
