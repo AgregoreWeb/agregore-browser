515503da45e24624c1c0115b98ce199a3fa8cc84
Mauve Signweaver <ranger@mauve.moe>
2026-03-02 18:03:21 -0500

    fix: set background white when needed (#323)
    * chore: MAke TypeScript see errors as errors
    
    * fix: set background to white where expected
    
    ---------
    
    Co-authored-by: Mauve Signweaver <contact@mauve.moe>

--- src/history.js
+++ src/history.js
@@ -1,21 +1,59 @@
+/**
+ * @typedef {() => Promise<import("electron").WebContents|null>} GetBackgroundPage
+ */
+/**
+ * @typedef {Object} HistoryItem
+ * @property {string} host
+ * @property {string} protocol
+ * @property {string} pathname
+ * @property {string} url
+ * @property {string} title
+ * @property {number} timestamp
+ */
+
+/**
+ * @type {GetBackgroundPage?}
+ */
 let getBackgroundPage = null
+/**
+ * @type {string?}
+ */
 let viewPage = null
 
+/**
+ * @param {GetBackgroundPage} backgroundPage 
+ */
 export function setGetBackgroundPage (backgroundPage) {
   getBackgroundPage = backgroundPage
 }
 
+/**
+ * 
+ * @param {string} page 
+ */
 export function setViewPage (page) {
   viewPage = page
 }
 
+/**
+ * @returns {string}
+ */
 export function getViewPage () {
+  if(!viewPage) throw new Error('View page not yet configured')
   return viewPage
 }
 
-export async function * search (query = '', ...args) {
-  const searchArgs = JSON.stringify([query, ...args]).slice(1, -1)
+/**
+ * 
+ * @param {string} query 
+ * @param {number} [maxResults]
+ * @returns {AsyncIterator<HistoryItem>}
+ */
+export async function * search (query = '', maxResults) {
+  if(!getBackgroundPage) throw new Error('History extension not yet configured')
+  const searchArgs = JSON.stringify([query, maxResults]).slice(1, -1)
   const webContents = await getBackgroundPage()
+  if(!webContents) throw new Error('History extension not yet configured')
   await webContents.executeJavaScript(`
     if(window.lastSearch?.return) {
       window.lastSearch?.return()
